2d9e5d3d251c8f909d24db4240529ad0ede9b907
chen60739 <892760739@qq.com>
2019-11-28 14:58:55 +0800

    Initial commit

---
+++ src/main/java/com/example/datong/DatongApplication.java
@@ -0,0 +1,13 @@
+package com.example.datong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatongApplication.class, args);
+    }
+
+}
