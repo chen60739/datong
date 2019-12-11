5048b7977f41fa6e3c43dad95808f1f8c692f7e8
chen60739 <892760739@qq.com>
2019-12-11 16:53:50 +0800

    service

---
+++ src/main/java/com/example/datong/service/CompanyService.java
@@ -0,0 +1,14 @@
+package com.example.datong.service;
+
+import com.example.datong.message.Result;
+import com.example.datong.model.CompanyRegistrationInfo;
+
+import javax.servlet.http.HttpServletRequest;
+
+public interface CompanyService {
+    void insert(CompanyRegistrationInfo company);
+
+    Result login(String username, String password, HttpServletRequest request);
+
+    boolean checkPhone(String phone);
+}
