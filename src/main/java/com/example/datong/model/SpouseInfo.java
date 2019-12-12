c0be97c930afb257361402170b18c10a57239be9
chen60739 <892760739@qq.com>
2019-12-12 13:20:40 +0800

    spouse最终

--- src/main/java/com/example/datong/model/SpouseInfo.java
+++ src/main/java/com/example/datong/model/SpouseInfo.java
@@ -4,21 +4,105 @@ public class SpouseInfo {
     private Integer id;
 
     private Integer pId;
-    //配偶姓名
+
     private String spouseName;
-    //配偶身份证号
+
     private String spouseIdCard;
 
     private Integer spouseCountyCode;
-    //现居住地地址
+
     private String spouseAddress;
-    //从业状况id
+
     private Integer spouseEmploymentStateId;
-    //工作单位
+
     private String spouseWorkUnit;
-    //联系电话
+
     private String spousePhone;
 
+    public Integer getId() {
+        return id;
+    }
 
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
+    public Integer getpId() {
+        return pId;
+    }
+
+    public void setpId(Integer pId) {
+        this.pId = pId;
+    }
+
+    public String getSpouseName() {
+        return spouseName;
+    }
+
+    public void setSpouseName(String spouseName) {
+        this.spouseName = spouseName;
+    }
+
+    public String getSpouseIdCard() {
+        return spouseIdCard;
+    }
+
+    public void setSpouseIdCard(String spouseIdCard) {
+        this.spouseIdCard = spouseIdCard;
+    }
+
+    public Integer getSpouseCountyCode() {
+        return spouseCountyCode;
+    }
+
+    public void setSpouseCountyCode(Integer spouseCountyCode) {
+        this.spouseCountyCode = spouseCountyCode;
+    }
+
+    public String getSpouseAddress() {
+        return spouseAddress;
+    }
+
+    public void setSpouseAddress(String spouseAddress) {
+        this.spouseAddress = spouseAddress;
+    }
+
+    public Integer getSpouseEmploymentStateId() {
+        return spouseEmploymentStateId;
+    }
+
+    public void setSpouseEmploymentStateId(Integer spouseEmploymentStateId) {
+        this.spouseEmploymentStateId = spouseEmploymentStateId;
+    }
+
+    public String getSpouseWorkUnit() {
+        return spouseWorkUnit;
+    }
+
+    public void setSpouseWorkUnit(String spouseWorkUnit) {
+        this.spouseWorkUnit = spouseWorkUnit;
+    }
+
+    public String getSpousePhone() {
+        return spousePhone;
+    }
+
+    public void setSpousePhone(String spousePhone) {
+        this.spousePhone = spousePhone;
+    }
+
+    @Override
+    public String toString() {
+        return "SpouseInfo{" +
+                "id=" + id +
+                ", pId=" + pId +
+                ", spouseName='" + spouseName + '\'' +
+                ", spouseIdCard='" + spouseIdCard + '\'' +
+                ", spouseCountyCode=" + spouseCountyCode +
+                ", spouseAddress='" + spouseAddress + '\'' +
+                ", spouseEmploymentStateId=" + spouseEmploymentStateId +
+                ", spouseWorkUnit='" + spouseWorkUnit + '\'' +
+                ", spousePhone='" + spousePhone + '\'' +
+                '}';
+    }
 }
