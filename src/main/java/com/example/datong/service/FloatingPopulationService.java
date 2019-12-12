9853976514a86160b407b9cb2fed139a60536983
user <571895781@qq.com>
2019-12-12 14:52:39 +0800

    floatingPopulationService

--- src/main/java/com/example/datong/service/FloatingPopulationService.java
+++ src/main/java/com/example/datong/service/FloatingPopulationService.java
@@ -19,10 +19,15 @@ public interface FloatingPopulationService {
 
     Map<String, Object> findChecking(Integer stateCode,Integer unitId);
 
+
+
+    List<NoPassedPerson> findNoPassed(Integer unitId);
+
+    //查询登记信息审核
+    Map<String,Object> findAll(String unitName,String name,String phone,String time1,String time2);
+
     //修改个人信息审核状态--通过--未通过
     int changePass(Integer id);
     Map<String,Object> changeNoPass(Integer id, CheckReason reason);
-
-    List<NoPassedPerson> findNoPassed(Integer unitId);
 
 }
