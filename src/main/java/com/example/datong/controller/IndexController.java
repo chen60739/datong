e28c9790c4df35bb759df739fc37c2fb22b6b1be
chen60739 <892760739@qq.com>
2019-12-13 14:49:10 +0800

    陈：前台控制层

--- src/main/java/com/example/datong/controller/IndexController.java
+++ src/main/java/com/example/datong/controller/IndexController.java
@@ -42,19 +42,12 @@ public class IndexController {
     }
 
     /**
-     * 登录检查通过后跳转主页
-     *
-     * @return 主页
+     * 返回前台主页
+     * @return
      */
-    @GetMapping("/main")
-    public String mainPage(Model model) {
-        HashMap<String, List<Dictionary>> dictionary = dictionaryService.getDictionary();
-        model.addAttribute("dictionary", dictionary);
-        List<AddressProvince> provinces = addressService.getProvice();
-        model.addAttribute("provinces", provinces);
-        List<AddressCounty> counties = addressService.getCountyByCityCode();
-        model.addAttribute("counties", counties);
-        return "register_person";
+    @RequestMapping("/main")
+    public String frontMain(){
+        return "front_main";
     }
 
 
@@ -68,8 +61,8 @@ public class IndexController {
 
     @GetMapping("/getCounty")
     @ResponseBody
-    public List<AddressCounty> getCounty() {
-        List<AddressCounty> counties = addressService.getCountyByCityCode();
+    public List<AddressCounty> getCounty(@RequestParam(name = "cityCode",defaultValue = "140200") String cityCode) {
+        List<AddressCounty> counties = addressService.getCountyByCityCode(cityCode);
         return counties;
     }
 
@@ -80,10 +73,26 @@ public class IndexController {
         return towns;
     }
 
+    /**
+     * 登录账户注册
+     * @param company
+     * @return 返回登录页面
+     */
     @PostMapping("/register")
     public String register(CompanyRegistrationInfo company) {
         companyService.insert(company);
         return "redirect:/";
     }
 
+    /**
+     * 注册页面手机号检查是否重复
+     * @param phone 手机号
+     * @return true为不重复，可用
+     */
+    @RequestMapping("/checkPhone")
+    @ResponseBody
+    public boolean checkUnitPhone(@RequestParam("phone") String phone){
+        boolean flag = companyService.checkPhone(phone);
+        return flag;
+    }
 }
